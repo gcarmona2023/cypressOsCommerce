d26fb51aeb45e7a75275bfa9eac4ed49db4502be
gcarmona2023 <gabriel.carmona@uniacc.edu>
2023-02-18 13:43:16 -0300

    Flujo de compra exitoso

--- cypress/e2e/example-spec.cy.js
+++ cypress/e2e/example-spec.cy.js
@@ -18,21 +18,37 @@ describe('osCommerce buying tablets samsung', () => {
         cy.visit('http://sqademosatp.net/catalog/index.php');
         cy.title().should('eq', expectedTitle);
         cy.screenshot();
+
+        //Buscar producto
         cy.get('[name="keywords"]').clear();
         cy.get('[name="keywords"]').type('Samsung galaxy tab{enter}');
         cy.get('a[id="tdb4"]').click();
+
+        //Actualizar cantidad
         cy.get('[name="cart_quantity[]"]').click().clear().type('2');
+        cy.get('.ui-button-text:contains("Update")').click();
+
+        //Realizar Checkout
         cy.get('a[id="tdb5"]').click();
-        //revisar la actualizacion del carro de compras
-        //cy.get('a[id="tdb6"]').click();
-        cy.get('[name="email_address"]').type(email + '{enter}');
-        cy.get('[name="password"]').type(pass + '{enter}');
+
+        //Iniciar sesión
+        cy.get('[name="email_address"]').type(email);
+        cy.get('[name="password"]').type(pass);
         cy.get('[id="tdb1"]').click();
+
+        //Continuar Shipping method
+        cy.get('.ui-button-text:contains("Continue")').click();
+
+        //Seleccionar tipo de pago
+        cy.get('input[name="payment"][value="cod"]').click();
+        //cy.wait(1000);
+        cy.get('.ui-button-text:contains("Continue")').click();
+
+        //Confirmar orden
+        cy.get('.ui-button-text:contains("Confirm Order")').click();
+
+        //Validar texto de cabecera
+        cy.get('h1').should('have.text', 'Your Order Has Been Processed!');
+        cy.get('.ui-button-text:contains("Continue")').click();
     });
 });
-/*
-describe('My second test'),
-    () => {
-        it('');
-    };
-*/
